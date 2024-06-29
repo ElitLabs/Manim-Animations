76888701e8772b6a79fd2d257f48d57f504f3dc3
Lior Pendler <lior.pendler@gmail.com>
2024-06-29 11:50:34 -0800

    Add more frames

--- cssheirarchy.py
+++ cssheirarchy.py
@@ -1,4 +1,12 @@
-from manim import Scene, Code, ReplacementTransform, ImageMobject, RIGHT, LEFT
+from manim import (
+    Scene,
+    Code,
+    Transform,
+    ReplacementTransform,
+    ImageMobject,
+    RIGHT,
+    LEFT,
+)
 import imgkit
 
 
@@ -24,58 +32,113 @@ html = """
 class Render(Scene):
     def render_imgs(self):
         imgkit.from_string(
-            html.replace("PLACEHOLDER", "one"),
-            "one.jpg",
+            html.replace("PLACEHOLDER", "1"),
+            "1.jpg",
             options=options,
-            css="cssheirarchy/one.css",
+            css="cssheirarchy/1.css",
         )
         imgkit.from_string(
-            html.replace("PLACEHOLDER", "two"),
-            "two.jpg",
+            html.replace("PLACEHOLDER", "2"),
+            "2.jpg",
             options=options,
-            css="cssheirarchy/two.css",
+            css="cssheirarchy/2.css",
         )
         imgkit.from_string(
-            html.replace("PLACEHOLDER", "three"),
-            "three.jpg",
+            html.replace("PLACEHOLDER", "3"),
+            "3.jpg",
             options=options,
-            css="cssheirarchy/three.css",
+            css="cssheirarchy/3.css",
+        )
+        imgkit.from_string(
+            html.replace("PLACEHOLDER", "4"),
+            "4.jpg",
+            options=options,
+            css="cssheirarchy/4.css",
+        )
+        imgkit.from_string(
+            html.replace("PLACEHOLDER", "5"),
+            "5.jpg",
+            options=options,
+            css="cssheirarchy/5.css",
+        )
+        imgkit.from_string(
+            html.replace("PLACEHOLDER", "6"),
+            "6.jpg",
+            options=options,
+            css="cssheirarchy/6.css",
         )
 
     def construct(self):
         self.render_imgs()
         one = Code(
-            "cssheirarchy/one.css",
+            "cssheirarchy/1.css",
             style="one-dark",
             font_size=18,
             line_spacing=0.5,
         ).to_edge(LEFT)
         two = Code(
-            "cssheirarchy/two.css",
+            "cssheirarchy/2.css",
             style="one-dark",
             font_size=18,
             line_spacing=0.5,
         ).to_edge(LEFT)
         three = Code(
-            "cssheirarchy/three.css",
+            "cssheirarchy/3.css",
+            style="one-dark",
+            font_size=18,
+            line_spacing=0.5,
+        ).to_edge(LEFT)
+        four = Code(
+            "cssheirarchy/4.css",
+            style="one-dark",
+            font_size=18,
+            line_spacing=0.5,
+        ).to_edge(LEFT)
+        five = Code(
+            "cssheirarchy/5.css",
+            style="one-dark",
+            font_size=18,
+            line_spacing=0.5,
+        ).to_edge(LEFT)
+        six = Code(
+            "cssheirarchy/6.css",
             style="one-dark",
             font_size=18,
             line_spacing=0.5,
         ).to_edge(LEFT)
 
-        one_img = ImageMobject("one.jpg").to_edge(RIGHT)
-        two_img = ImageMobject("two.jpg").to_edge(RIGHT)
-        three_img = ImageMobject("three.jpg").to_edge(RIGHT)
+        one_img = ImageMobject("1.jpg").to_edge(RIGHT)
+        two_img = ImageMobject("2.jpg").to_edge(RIGHT)
+        three_img = ImageMobject("3.jpg").to_edge(RIGHT)
+        four_img = ImageMobject("4.jpg").to_edge(RIGHT)
+        five_img = ImageMobject("5.jpg").to_edge(RIGHT)
+        six_img = ImageMobject("6.jpg").to_edge(RIGHT)
 
         self.add(one_img)
         self.add(one)
         self.wait(1)
         self.play(
-            ReplacementTransform(one, two, run_time=2),
-            ReplacementTransform(one_img, two_img, run_time=2),
+            Transform(one, two, run_time=2),
+            Transform(one_img, two_img, run_time=2),
         )
         self.wait(1)
         self.play(
-            ReplacementTransform(two, three, run_time=2),
-            ReplacementTransform(two_img, three_img, run_time=2),
+            ReplacementTransform(one, three, run_time=2),
+            ReplacementTransform(one_img, three_img, run_time=2),
         )
+        self.wait(1)
+        self.play(
+            ReplacementTransform(three, four, run_time=2),
+            ReplacementTransform(three_img, four_img, run_time=2),
+        )
+        self.wait(1)
+        self.play(
+            ReplacementTransform(four, five, run_time=2),
+            ReplacementTransform(four_img, five_img, run_time=2),
+        )
+        self.wait(1)
+        self.play(
+            ReplacementTransform(five, six, run_time=2),
+            ReplacementTransform(five_img, six_img, run_time=2),
+        )
+        self.wait(1)
